a5e71c3e4599a1e4102808acfcb627b152443797
Freist Li <freistli@microsoft.com>
2021-03-01 16:42:57 +0800

    Build update in VS 16.8.6

--- C++/SimpleAppWithWinUI/SimpleApp/SimpleApp/SimpleApp.h
+++ C++/SimpleAppWithWinUI/SimpleApp/SimpleApp/SimpleApp.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "resource.h"
- 
+//Build in VS 16.8.6, reported std::back_insert error. Add additional headers
+#include <algorithm>
+#include <iterator>
+#include <set>
+
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.system.h>
 #include <winrt/windows.ui.xaml.hosting.h>
